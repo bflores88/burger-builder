2e401d2ba7a91c6365dfe97ee81d1abde5f8de96
kmietotwy <kmietotwy@gmail.com>
2019-07-31 14:36:45 -0700

    logo in toolbar and sidebar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -4,13 +4,15 @@ import Logo from '../../../components/Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
-  <header className={classes.Toolbar}>
-    <div>MENU</div>
-    <Logo />
-    <nav>
-      <NavigationItems />
-    </nav>
-  </header>
+	<header className={classes.Toolbar}>
+		<div>MENU</div>
+		<div className={classes.Logo}>
+			<Logo />
+		</div>
+		<nav>
+			<NavigationItems />
+		</nav>
+	</header>
 );
 
 export default toolbar;
