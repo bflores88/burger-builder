3ca62001c2eea4281fd94da4de4fe025d3677258
kmietotwy <kmietotwy@gmail.com>
2019-08-02 12:01:05 -0700

    added componentWillUnmount to remove axios interceptors after not needing them anymore

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -6,17 +6,25 @@ const withErrorHandler = (WrappedComponent, axios) => {
 	return class extends Component {
 		state = {
 			error: null,
-    };
-    
+		};
+
 		componentWillMount() {
-			axios.interceptors.request.use(req => {
-        this.setState({ error: null });
-        return req;
+			this.reqInterceptor = axios.interceptors.request.use((req) => {
+				this.setState({ error: null });
+				return req;
 			});
-			axios.interceptors.response.use(res => res, error => {
-        this.setState({ error: error });
-			});
-		}
+			this.resInterceptor = axios.interceptors.response.use(
+				(res) => res,
+				(error) => {
+					this.setState({ error: error });
+				},
+			);
+    }
+    
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
 
 		errorConfirmedHandler = () => {
 			this.setState({ error: null });
@@ -25,11 +33,9 @@ const withErrorHandler = (WrappedComponent, axios) => {
 		render() {
 			return (
 				<Aux>
-          <Modal
-            show={this.state.error}
-            modalClosed={this.errorConfirmedHandler}>
-            {this.state.error ? this.state.error.message : null}
-            </Modal>
+					<Modal show={this.state.error} modalClosed={this.errorConfirmedHandler}>
+						{this.state.error ? this.state.error.message : null}
+					</Modal>
 					<WrappedComponent {...this.props} />
 				</Aux>
 			);
