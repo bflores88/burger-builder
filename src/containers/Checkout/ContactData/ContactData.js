12636087acdf8da9965d1909e25ce1a8718ea341
kmietotwy <kmietotwy@gmail.com>
2019-08-05 15:01:29 -0700

    two way binding on input forms by immutably duplicating state

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,70 +7,132 @@ import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
-  state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
-    },
-    loading: false,
+	state = {
+		orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+			street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+			zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP'
+        },
+        value: ''
+      },
+			country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+			email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your Email'
+        },
+        value: ''
+      },
+			deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' },
+          ]
+        },
+        value: ''
+      },
+		},
+		loading: false,
+	};
 
-  }
-
-  orderHandler = (event) => {
-    event.preventDefault();
-    console.log(this.props.ingredients)
-    this.setState({ loading: true });
+	orderHandler = (event) => {
+		event.preventDefault();
+		console.log(this.props.ingredients);
+		this.setState({ loading: true });
 		const order = {
 			ingredients: this.props.ingredients,
 			// Note: total price should be calculated on the server to ensure the total price is not manipulated on the front-end!!
 			price: this.props.price,
-			customer: {
-				name: 'Brenda',
-				address: {
-					street: 'test',
-					zipCode: '23123',
-					country: 'US',
-				},
-				email: 'test@test.com',
-				deliveryMethod: 'fastest',
-			},
 		};
 		axios
 			.post('/orders.json', order)
 			.then((response) => {
-        this.setState({ loading: false });
-        this.props.history.push('/')
+				this.setState({ loading: false });
+				this.props.history.push('/');
 			})
 			.catch((error) => {
 				this.setState({ loading: false });
 				console.log('error', error);
-    	});
+			});
+  };
+  
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    }
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm})
   }
 
   render() {
-    let form = (
-      <form>
-        <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
-        <Input inputtype="input" type="email " name="email" placeholder="Your Email" />
-        <Input inputtype="input" type="text" name="street" placeholder="Street" />
-        <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
-        <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
-      </form>
-    );
-    if (this.state.loading) {
-      form = <Spinner />
-    };
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      })
+    }
 
-    return (
-      <div className={classes.ContactData}>
-        <h4>Enter your Contact Data</h4>
-        {form}
-      </div>
+		let form = (
+			<form>
+        {/* <Input elementType="..." elementConfig="..." value="..." /> */}
+        {formElementsArray.map(formElement => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+            changed={(event) => this.inputChangedHandler(event, formElement.id)}
+          />
+        ))}
+				<Button btnType="Success" clicked={this.orderHandler}>
+					ORDER
+				</Button>
+			</form>
+		);
+		if (this.state.loading) {
+			form = <Spinner />;
+		}
 
-    )
-  }
+		return (
+			<div className={classes.ContactData}>
+				<h4>Enter your Contact Data</h4>
+				{form}
+			</div>
+		);
+	}
 }
 
 export default ContactData;
