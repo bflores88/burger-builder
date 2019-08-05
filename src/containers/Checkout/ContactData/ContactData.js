c51b7eb527ae2f2e84d78147c1ad119b3c8dc1fd
kmietotwy <kmietotwy@gmail.com>
2019-08-05 16:19:34 -0700

    form input validation and styling

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -15,7 +15,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+
+        },
+        valid: false
       },
 			street: {
         elementType: 'input',
@@ -23,7 +28,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Street'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          
+        },
+        valid: false
       },
 			zipCode: {
         elementType: 'input',
@@ -31,7 +41,13 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'ZIP'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5
+        },
+        valid: false
       },
 			country: {
         elementType: 'input',
@@ -39,7 +55,12 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          
+        },
+        valid: false
       },
 			email: {
         elementType: 'input',
@@ -47,7 +68,12 @@ class ContactData extends Component {
           type: 'email',
           placeholder: 'Your Email'
         },
-        value: ''
+        value: '',
+        validation: {
+          required: true,
+          
+        },
+        valid: false
       },
 			deliveryMethod: {
         elementType: 'select',
@@ -57,7 +83,7 @@ class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest' },
           ]
         },
-        value: ''
+        value: 'fastest',
       },
 		},
 		loading: false,
@@ -66,11 +92,16 @@ class ContactData extends Component {
 	orderHandler = (event) => {
 		event.preventDefault();
 		console.log(this.props.ingredients);
-		this.setState({ loading: true });
+    this.setState({ loading: true });
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
 		const order = {
 			ingredients: this.props.ingredients,
 			// Note: total price should be calculated on the server to ensure the total price is not manipulated on the front-end!!
-			price: this.props.price,
+      price: this.props.price,
+      orderData: formData,
 		};
 		axios
 			.post('/orders.json', order)
@@ -83,6 +114,24 @@ class ContactData extends Component {
 				console.log('error', error);
 			});
   };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    return isValid;
+  }
   
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
@@ -92,7 +141,9 @@ class ContactData extends Component {
       ...updatedOrderForm[inputIdentifier]
     }
     updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log(updatedFormElement);
     this.setState({orderForm: updatedOrderForm})
   }
 
@@ -106,7 +157,7 @@ class ContactData extends Component {
     }
 
 		let form = (
-			<form>
+			<form onSubmit={this.orderHandler}>
         {/* <Input elementType="..." elementConfig="..." value="..." /> */}
         {formElementsArray.map(formElement => (
           <Input
@@ -114,6 +165,8 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
